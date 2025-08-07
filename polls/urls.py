898cf5e4e3fc59381ab9f3eddc5de7b463ffd494
kyreniabupt <Kyreniaws@163.com>
2025-08-07 12:38:53 +0800

    update query

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import get_all_stocks
 
 router = DefaultRouter()
-router.register(r'stocks', get_all_stocks)
+router.register(r'stocks', get_all_stocks,basename='stocks')
 
 urlpatterns = [
     path('', include(router.urls)),
--- polls/views.py
+++ polls/views.py
@@ -5,10 +5,22 @@ from rest_framework.pagination import PageNumberPagination
 
 class StockPagination(PageNumberPagination):
     page_size = 10
-    page_size_query_param = 'page_size'  # 支持客户端指定每页大小
-    max_page_size = 1000
+    page_size_query_param = 'page_size'
+    max_page_size = 3000
 
 class get_all_stocks(viewsets.ModelViewSet):
-    queryset = Stock.objects.all().order_by('-date')
     serializer_class = StockSerializer
     pagination_class = StockPagination
+
+    def get_queryset(self):
+        queryset = Stock.objects.all().order_by('date')
+        start_date = self.request.query_params.get('start_date')
+        end_date = self.request.query_params.get('end_date')
+
+        if start_date:
+            queryset = queryset.filter(date__gte=start_date)
+        if end_date:
+            queryset = queryset.filter(date__lte=end_date)
+
+        return queryset
+
